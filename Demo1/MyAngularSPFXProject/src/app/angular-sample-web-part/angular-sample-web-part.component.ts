078e30de5e944e248255bb15f4760d4a382714e5
Raphilass <phil@thecoulsonfamily.co.uk>
2019-06-04 12:07:18 +0100

    Updates

--- Demo1/MyAngularSPFXProject/src/app/angular-sample-web-part/angular-sample-web-part.component.ts
+++ Demo1/MyAngularSPFXProject/src/app/angular-sample-web-part/angular-sample-web-part.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/core';
 import * as moment from 'moment';
+import { sp } from '@pnp/sp';
 
 @Component({
   selector: 'app-angular-sample-web-part',
@@ -11,6 +12,7 @@ export class AngularSampleWebPartComponent implements OnInit, AfterViewInit {
 
   // Input Variables for Angular Element
   @Input("description") description: string;
+  @Input('siteurl') siteUrl: string;
 
 
   constructor() {
@@ -18,13 +20,29 @@ export class AngularSampleWebPartComponent implements OnInit, AfterViewInit {
     moment.locale("en-GB")
   }
 
+  lists:any[];
 
   ngOnInit() {
-    
+    sp.web.lists.get().then(lists =>{
+      if(lists.length > 0){
+        this.lists = lists
+      } else {
+        this.lists.push('No Lists found ont his site.')
+      }
+    })
   }
 
   ngAfterViewInit(): void {
-
+    sp.setup({
+      sp: {
+        headers: {
+          'Pragma': 'no-cache',
+          'Cache-control': 'no-cache'
+        },
+        baseUrl: this.siteUrl
+      },
+      globalCacheDisable: true
+    })
   }
 
 }
